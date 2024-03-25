4088cbf7202f8cd8c62162c3ec99f053811d1485
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2024-03-25 23:31:45 +0500

    Add all changes
    except all changes

---
+++ todo.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    let answer = await inquirer.prompt([
+        {
+            name: "TODO",
+            type: "input",
+            message: "Enter your Todo",
+        },
+        {
+            name: "Addmore",
+            type: "confirm",
+            message: "what do addmore Todo ",
+            default: false
+        },
+    ]);
+    const { TODO, Addmore } = answer;
+    console.log(answer);
+    loop = Addmore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("kindly Add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your Todo List ");
+    todos.forEach(TODO => {
+        console.log(TODO);
+    });
+}
+else {
+    console.log("No Todo Found");
+}
