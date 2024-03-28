2b50690723bf9df0d46fabbbf13dac1bd1dc019f
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2024-03-28 23:15:14 +0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,17 @@
+#!  /usr/bin/env node
 import inquirer from "inquirer";
+import chakAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function atm() {
+    let rainbow = chakAnimation.rainbow(`let Start Todo List`);
+    await sleep();
+    rainbow.stop();
+}
+atm();
 let todos = [];
 let loop = true;
 while (loop) {
@@ -24,13 +37,13 @@ while (loop) {
     else {
         console.log("kindly Add valid input");
     }
-}
-if (todos.length > 0) {
-    console.log("Your Todo List ");
-    todos.forEach(TODO => {
-        console.log(TODO);
-    });
-}
-else {
-    console.log("No Todo Found");
+    if (todos.length > 0) {
+        console.log("Your Todo List ");
+        todos.forEach(TODO => {
+            console.log(TODO);
+        });
+    }
+    else {
+        console.log("No Todo Found");
+    }
 }
